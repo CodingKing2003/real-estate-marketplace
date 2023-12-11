6948f4e13b7ac5b3aa3d7692908796f958f24561
Rajan <rajanj448@student.sfit.ac.in>
2023-12-11 14:25:56 +0530

    Complete sign in page functionality

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -47,10 +47,13 @@ export const signin = async (req, res, next) => {
       return next(errorHandler(401, "Invalid credentials"));
     }
 
-    const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
 
-    res.cookie('access_token',token,{httpOnly:true}).status(201).json(rest)
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(201)
+      .json(rest);
   } catch (error) {
     next(error);
   }
