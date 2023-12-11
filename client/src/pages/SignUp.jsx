e65ff299541e03a33fe6b7ac928517c08452576f
Rajan <rajanj448@student.sfit.ac.in>
2023-12-11 13:20:46 +0530

    Complete sign up page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -5,7 +5,7 @@ const SignUp = () => {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-  const  navigate=useNavigate();
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -24,18 +24,15 @@ const SignUp = () => {
       }
 
       const data = await res.json();
-     
 
       if (data.success == false) {
         setLoading(false);
         setError(data.message);
-        
-
       }
-      setLoading(false); 
+      setLoading(false);
       console.log(data);
       setError(null);
-      navigate('/sign-in');
+      navigate("/sign-in");
     } catch (error) {
       setLoading(false);
       setError(error.message);
@@ -48,7 +45,7 @@ const SignUp = () => {
       [e.target.id]: e.target.value,
     });
   };
- 
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7 ">Sign Up</h1>
@@ -74,8 +71,11 @@ const SignUp = () => {
           id="password"
           onChange={handleChange}
         />
-        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80" >
-          {loading?'Loading...':"Sign Up"}
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -84,7 +84,7 @@ const SignUp = () => {
           <span className="text-blue-700">Sign In</span>
         </Link>
       </div>
-      {error && <p className='text-red-500 mt-5'>{error}</p>}
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 };
